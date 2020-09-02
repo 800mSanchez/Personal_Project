abf357236d68ceaa0c61589e02fe09a594365b4d
Alejandro Sanchez <alejandrosanchez9494@gmail.com>
2020-09-02 13:20:52 -0500

    CSS

---
+++ src/Components/StoreFront/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+/* import "./Header.css"; */
+import {Link} from "react-router-dom";
+import {connect} from 'react-redux';
+import {logoutUser, getUser} from '../../Store/reducer';
+import axios from 'axios';
+
+class Header extends React.Component {
+
+    componentDidMount(){
+        this.props.getUser();
+    }
+
+    logout = () => {
+        axios.get('/auth/logout').then( res => {
+            this.props.logoutUser();
+            this.props.history.push('/');
+        }).catch(err => console.log(err))
+    }
+
+    render(){
+        return <header>
+            <div className='header-container'>
+            <h4>Locale</h4>
+                 <button className="logout-btn" onClick={this.logout}><Link to="/">Logout</Link>/</button>
+            </div>
+        </header>
+    }
+}
+
+const mapStateToProps = state => state;
+
+export default connect(mapStateToProps, {logoutUser, getUser})(Header);
--- src/Components/StoreFront/StoreFront.js
+++ src/Components/StoreFront/StoreFront.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import './StoreFront.css'
+import Header from './Header';
 
 class StoreFront extends React.Component {
   constructor(props) {
@@ -27,7 +28,6 @@ class StoreFront extends React.Component {
     const { inventory } = this.state;
     axios.post('./store/product', { inventory }).then(res => {
       this.props.addToCart(res.data);
-      this.props.history.push('/cart')
     }).catch(err => {
       console.log(err);
       alert('Transfer To Cart Failed')
@@ -38,17 +38,19 @@ class StoreFront extends React.Component {
   render() {
     console.log(this.state.inventory)
     let allInventory = this.state.inventory.map(e => {
-      return <div className="product-area">
-        <div>{e.title} {e.price}</div>
-        <div>{e.location}</div>
-        <div>{e.description}</div>
+      return <div>
+        {e.title} {e.price}
+        {e.location}
+        {e.description}
         <img className="image" src={e.image} alt="a cherry oak chair" />
         <button onClick={this.addToCart}>Add To Cart</button>
       </div>
     })
-    return <div className="storeFront">
-      <div>Shop Local Nationwide!</div>
-      <div>
+    return <div className="storeContainer">
+      <div className='header'>
+        <Header/>
+      </div>
+      <div className="inventory">
         {allInventory}
       </div>
     </div>
